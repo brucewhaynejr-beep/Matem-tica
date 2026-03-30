6dc006ee5ffc1004532996dd4d8a6a71bea4f50d
brucewhaynejr-beep <brucewhaynejr@gmail.com>
2026-03-30 20:52:50 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+  const nextConfig = {                                                                                                                                                                                 
+    // Definir basePath para o GitHub Pages                                                                                                                                                            
+    basePath: "/Mat7ano", // ajuste se o nome do repositório mudar                                                                                                                                     
+    trailingSlash: true,                 
+  };
+                                                                                                                                                                                                       
+  module.exports = nextConfig;
